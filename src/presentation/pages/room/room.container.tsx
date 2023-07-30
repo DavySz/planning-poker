cc3b7fd8d7490f15b49de8407e67b5f085cb445c
Davy Assunção <davydesouzabar@gmail.com>
2023-07-30 13:47:26 -0400

    feat: create initial room component

---
+++ src/presentation/pages/room/room.container.tsx
@@ -0,0 +1,10 @@
+import { PageTemplate } from "@presentation/components";
+import { RoomUI } from "./room.ui";
+
+export const Room: React.FC = () => {
+  return (
+    <PageTemplate onClick={() => {}} label="">
+      <RoomUI />
+    </PageTemplate>
+  );
+};
---
+++ src/presentation/pages/room/room.ui.tsx
@@ -0,0 +1,5 @@
+import { Card } from "@presentation/components/card/card";
+
+export const RoomUI: React.FC = () => {
+  return <Card />;
+};
