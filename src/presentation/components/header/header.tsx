0618f2fe3be5049c771a2f008e0c1a8ab33046c9
Davy Assunção <davydesouzabar@gmail.com>
2023-07-30 13:45:25 -0400

    feat: create initial header component

---
+++ src/presentation/components/header/header.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../button/button";
+import { IHeader } from "./header.types";
+
+export const Header: React.FC<IHeader> = ({ onClick, label }) => {
+  return (
+    <div className="flex justify-between items-center p-8 bg-blue-500">
+      <h1 className="font-mono font-semibold text-4xl text-white">
+        Planning Poker
+      </h1>
+      <Button onClick={onClick}>{label}</Button>
+    </div>
+  );
+};
---
+++ src/presentation/components/header/header.types.ts
@@ -0,0 +1,4 @@
+export type IHeader = {
+  onClick: () => void;
+  label: string;
+};
