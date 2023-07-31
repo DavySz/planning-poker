7205f4e664672196e78ef988ccc06ff9ce23d260
Davy Assunção <davydesouzabar@gmail.com>
2023-07-31 15:21:09 -0400

    feat: use onChange props

--- src/presentation/components/input-selector/input-selector.tsx
+++ src/presentation/components/input-selector/input-selector.tsx
@@ -3,6 +3,17 @@ import Select from "react-select";
 import { IInputSelector } from "./input-selector.types";
 import { customStyles } from "./input-selector.styles";
 
-export const InputSelector: React.FC<IInputSelector> = ({ options }) => {
-  return <Select options={options} styles={customStyles} />;
+export const InputSelector: React.FC<IInputSelector> = ({
+  placeholder,
+  onChange,
+  options,
+}) => {
+  return (
+    <Select
+      options={options}
+      styles={customStyles}
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
 };
--- src/presentation/components/input-selector/input-selector.types.ts
+++ src/presentation/components/input-selector/input-selector.types.ts
@@ -1,3 +1,5 @@
+import { ActionMeta, SingleValue } from "react-select";
+
 export type TOption = {
   value: string;
   label: string;
@@ -5,4 +7,9 @@ export type TOption = {
 
 export interface IInputSelector {
   options: TOption[];
+  placeholder: string;
+  onChange: (
+    newValue: SingleValue<TOption>,
+    actionMeta: ActionMeta<TOption>
+  ) => void;
 }
