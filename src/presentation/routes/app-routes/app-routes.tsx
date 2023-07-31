88976a104fb891e3acb92c5939595320d50a7a3b
Davy Assunção <davydesouzabar@gmail.com>
2023-07-31 15:19:32 -0400

    feat: add key in Route

--- src/presentation/routes/app-routes/app-routes.tsx
+++ src/presentation/routes/app-routes/app-routes.tsx
@@ -4,7 +4,7 @@ import { appRoutes } from "./app-routes.register";
 export const AppRoutes = () => {
   const getRoutes = () => {
     return appRoutes.map(({ element, path }) => {
-      return <Route element={element} path={path} />;
+      return <Route element={element} path={path} key={path} />;
     });
   };
 
