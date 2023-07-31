eca95cacce4ea98ccd9221038ea255d5b3b5a518
Davy Assunção <davydesouzabar@gmail.com>
2023-07-31 12:09:35 -0400

    feat: import inputs components

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -2,3 +2,5 @@ export * from "./button/button";
 export * from "./card/card.container";
 export * from "./header/header";
 export * from "./page-template/page-template";
+export * from "./input/input";
+export * from "./input-selector/input-selector";
