b7ab426e6a391f6b0df3bcb7d4ef0980fbdadba7
Davy Assunção <davydesouzabar@gmail.com>
2023-08-01 15:38:43 -0400

    feat: create websocket listener

--- src/infra/protocols/firebase/firebase-database-adapter/firebase-database-adapter.ts
+++ src/infra/protocols/firebase/firebase-database-adapter/firebase-database-adapter.ts
@@ -7,31 +7,37 @@ import {
   ref,
   onValue,
   getDatabase,
+  get as getFirebase,
   set as setFirebase,
   push as pushFirebase,
+  DataSnapshot,
 } from "firebase/database";
 
 export class FirebaseDatabaseAdapter
   implements ISetDatabase, IGetDatabase, IPushDatabase
 {
+  private readonly database = getDatabase();
+
   async set(data: any, path: string): Promise<void> {
-    const database = getDatabase();
-    const databaseRef = ref(database, path);
+    const databaseRef = ref(this.database, path);
     await setFirebase(databaseRef, data);
   }
 
-  async get(path: string, callback: (data: any) => void): Promise<any> {
-    const database = getDatabase();
-    const databaseRef = ref(database, path);
+  async lister(path: string, callback: (data: any) => void): Promise<any> {
+    const databaseRef = ref(this.database, path);
     onValue(databaseRef, (snapshot) => {
       const data = snapshot.val();
       callback(data);
     });
   }
 
+  async get(path: string): Promise<DataSnapshot> {
+    const response = await getFirebase(ref(this.database, path));
+    return response;
+  }
+
   async push(data: any, path: string): Promise<string> {
-    const database = getDatabase();
-    const databaseRef = ref(database, path);
+    const databaseRef = ref(this.database, path);
     const response = await pushFirebase(databaseRef, data);
     return response.key!;
   }
