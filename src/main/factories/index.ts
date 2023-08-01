2ccd1d369a7e4d1056494a37ca5011eb209d8bcd
Davy Assunção <davydesouzabar@gmail.com>
2023-08-01 12:11:00 -0400

    feat: export all factories

---
+++ src/main/factories/index.ts
@@ -0,0 +1,5 @@
+export * from "./firebase-database-adapter-factory/firebase-database-adapter-factory";
+export * from "./uuid-hash-adapter-factory/uuid-hash-adapter-factory";
+export * from "./room-factory/room-factory.types";
+export * from "./room-factory/room-factory";
+export * from "./user-factory/user-factory";
