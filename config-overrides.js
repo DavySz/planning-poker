0844afbe9068a1589a0b673e54789433dd4326aa
Davy Assunção <davydesouzabar@gmail.com>
2023-08-01 12:02:16 -0400

    feat: register new absolute paths

--- config-overrides.js
+++ config-overrides.js
@@ -4,5 +4,8 @@ const path = require('path');
 module.exports = override(
   addWebpackAlias({
     '@presentation': path.resolve(__dirname, 'src/presentation'),
+    '@infra': path.resolve(__dirname, 'src/infra'),
+    '@main': path.resolve(__dirname, 'src/main'),
+    '@data': path.resolve(__dirname, 'src/data'),
   })
 );
