9109c2489cc9ffbd0b85ca310d07723ad006ae32
Davy Assunção <davydesouzabar@gmail.com>
2023-08-05 19:19:35 -0400

    style: update card interface

--- src/presentation/components/select-card/select-card.ui.tsx
+++ src/presentation/components/select-card/select-card.ui.tsx
@@ -5,16 +5,20 @@ export const SelectCardUI: React.FC<ISelectCardUI> = ({
   onClick,
   option,
 }) => {
-  const selectedStyle = isSelected ? "bg-blue-600" : "";
+  const selectedStyle = isSelected
+    ? "bg-blue-600 -translate-y-2 transition-translate: duration-500"
+    : "";
+
+  const selectedTextStyle = isSelected ? "text-white" : "text-black";
 
   return (
     <div className="h-20 w-14 cursor-pointer" onClick={onClick}>
       <div
         className={`flex flex-col items-center justify-center w-[100%] h-[100%] rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] ${selectedStyle}`}
       >
-        {!isSelected && (
-          <span className="text-black font-medium text-lg">{option}</span>
-        )}
+        <span className={`${selectedTextStyle} font-medium text-lg`}>
+          {option}
+        </span>
       </div>
     </div>
   );
