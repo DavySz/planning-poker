2d1516dff03b70c8b49bd68c362f25faada48bf1
Davy Assunção <davydesouzabar@gmail.com>
2023-08-05 00:56:13 -0400

    feat: config domain absolute path

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = override(
   addWebpackAlias({
     '@presentation': path.resolve(__dirname, 'src/presentation'),
+    '@domain': path.resolve(__dirname, 'src/domain'),
     '@infra': path.resolve(__dirname, 'src/infra'),
     '@main': path.resolve(__dirname, 'src/main'),
     '@data': path.resolve(__dirname, 'src/data'),
