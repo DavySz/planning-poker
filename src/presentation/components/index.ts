0c9db25c7c912f3cf1ab55cb0396a3556f8e2aaf
Davy Assunção <davydesouzabar@gmail.com>
2023-08-05 19:18:38 -0400

    feat: create timer component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -5,3 +5,4 @@ export * from "./page-template/page-template";
 export * from "./input/input";
 export * from "./input-selector/input-selector";
 export * from "./loading/loading";
+export * from "./timer/timer.container";
