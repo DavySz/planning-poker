1c04f6c6bf07e3623174c7e8157619dd50182153
Davy Assunção <davydesouzabar@gmail.com>
2023-08-07 16:54:23 -0400

    feat: add custom component prop

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,12 +1,17 @@
 import { Button } from "../button/button";
 import { IHeader } from "./header.types";
 
-export const Header: React.FC<IHeader> = ({ onClick, label }) => {
+export const Header: React.FC<IHeader> = ({
+  label,
+  onClick,
+  customComponent,
+}) => {
   return (
     <div className="flex justify-between items-center p-8 bg-blue-500">
       <h1 className="font-mono font-semibold text-4xl text-white">
         Planning Poker
       </h1>
+      {customComponent && customComponent}
       {onClick && <Button onClick={onClick}>{label}</Button>}
     </div>
   );
--- src/presentation/components/header/header.types.ts
+++ src/presentation/components/header/header.types.ts
@@ -1,4 +1,7 @@
+import { ReactNode } from "react";
+
 export type IHeader = {
+  customComponent?: ReactNode;
   onClick?: () => void;
   label?: string;
 };
