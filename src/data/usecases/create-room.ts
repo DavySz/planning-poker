0fc026ac1ce042e29c504f0d81fbb86e48b30580
Davy Assunção <davydesouzabar@gmail.com>
2023-08-08 00:44:34 -0400

    feat: add create room usecase

---
+++ src/data/usecases/create-room.ts
@@ -0,0 +1,11 @@
+import { IDatabase } from "@data/protocols/database";
+import { CreateRoom, CreateRoomSpace } from "@domain/usecases";
+
+export class RemoteCreateRoom implements CreateRoom {
+  constructor(private readonly database: IDatabase) {}
+
+  async create(params: CreateRoomSpace.Params): Promise<CreateRoomSpace.Model> {
+    const { key } = await this.database.push(params.room, "rooms");
+    return key;
+  }
+}
