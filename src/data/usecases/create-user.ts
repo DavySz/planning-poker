58c2a12eb343c3102aa4b45f02d65d2143b14208
Davy Assunção <davydesouzabar@gmail.com>
2023-08-08 14:38:59 -0400

    feat: create remote create user factory

---
+++ src/data/usecases/create-user.ts
@@ -0,0 +1,10 @@
+import { IDatabase } from "@data/protocols/database";
+import { CreateUser, CreateUserSpace } from "@domain/usecases/create-user";
+
+export class RemoteCreateUser implements CreateUser {
+  constructor(private readonly database: IDatabase) {}
+
+  async create({ room, user }: CreateUserSpace.Params): Promise<void> {
+    await this.database.push(user, `rooms/${room}/users`);
+  }
+}
