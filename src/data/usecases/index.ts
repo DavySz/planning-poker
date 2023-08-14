1505c10e050fadc8e2eeae06df2609afca53881d
davy <davydesouzabar@gmail.com>
2023-08-14 16:42:09 -0400

    feat: export all usecases

--- src/data/usecases/index.ts
+++ src/data/usecases/index.ts
@@ -1,1 +1,11 @@
 export * from "./create-room";
+export * from "./create-user";
+
+export * from "./get-room";
+export * from "./get-all-users";
+
+export * from "./update-user";
+export * from "./update-room";
+
+export * from "./update-all-users";
+export * from "./get-room-events";
