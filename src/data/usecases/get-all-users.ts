9abb37610edfedcfc4b55f73970ec6a48a48d7a7
davy <davydesouzabar@gmail.com>
2023-08-14 16:41:15 -0400

    feat: create RemoteGetAllUsers usecases

---
+++ src/data/usecases/get-all-users.ts
@@ -0,0 +1,11 @@
+import { IDatabase } from "@data/protocols/database";
+import { GetAllUsers, GetAllUsersSpace } from "@domain/usecases";
+
+export class RemoteGetAllUsers implements GetAllUsers {
+  constructor(private readonly database: IDatabase) {}
+
+  async get(room: string): Promise<GetAllUsersSpace.Model> {
+    const user = await this.database.get(`rooms/${room}/users`);
+    return user.val();
+  }
+}
