69a58db5bcc5c31a4cd1f64569cc8f3ab0498ba5
davy <davydesouzabar@gmail.com>
2023-09-01 22:51:47 -0400

    refactor: remove no unused vars

--- src/presentation/components/card/card.container.tsx
+++ src/presentation/components/card/card.container.tsx
@@ -1,6 +1,5 @@
 import { ICard } from "./card.types";
 import { CardUI } from "./card.ui";
-import _ from "lodash";
 
 export const Card: React.FC<ICard> = ({ visible, option }) => {
   return <CardUI visible={visible} option={option} />;
