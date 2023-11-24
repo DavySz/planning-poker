856ce82727365b66de7a24f15f757c5702a47ed0
davy <davydesouzabar@gmail.com>
2023-11-24 09:51:21 -0400

    feat: add fibonacci voting

--- src/presentation/pages/create-room/create-room.options.tsx
+++ src/presentation/pages/create-room/create-room.options.tsx
@@ -1,1 +1,4 @@
-export const options = [{ value: "1", label: "custom (PP, P, M, G, GG)" }];
+export const options = [
+  { value: "1", label: "custom (PP, P, M, G, GG)" },
+  { value: "2", label: "fibonacci (1, 1, 2, 3, 5, 8, 13)" },
+];
